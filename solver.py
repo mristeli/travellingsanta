d8f6563662558d5f390475dcd3a0e4289a91fd2c
Matti Risteli <matti.risteli@iki.fi>
2018-12-19 23:28:22 +0200

    solver.py: traverses through the whole search tree most stupidly

--- solver.py
+++ solver.py
@@ -1,11 +1,11 @@
-#!/usr/bin/local/python3
+#!/usr/local/bin/python3
 from math import sin, cos, sqrt, atan2, radians
 from functools import reduce
+import sys
+import itertools
+import functools
 
-print("Santa!")
-
-home = (68.07361, 29.31527)
-
+print('Santa is on the way!')
 
 def distance(point1, point2):
     r = 6378
@@ -23,15 +23,62 @@ def distance(point1, point2):
     return r * c
 
 def run_around_distance(route):
+    home = (68.07361, 29.31527)
     route_acc = reduce(
-        lambda acc, point: (point, acc[1] + distance(point, acc[0])),
+        lambda acc, point: (point[1], acc[1] + distance(point[1], acc[0])),
             route, (home, 0))
     return route_acc[1] + distance(home, route_acc[0])
-    
-    
 
-print("Home - p1 - home:" + str(distance(home, (40.733034,-86.762233)) * 2))
-print("moi: " + str(run_around_distance(
-    [(40.733034,-86.762233)]
-    )))
+def split_to_runs(perm, weight_limit = 10000000):
+    all_runs = [] 
+    run = []
+    current_limit = weight_limit
+    for entry in perm:
+        if (current_limit >= entry[1]): 
+            run.append((entry[0], entry[2]))
+            current_limit = current_limit - entry[1]
+        else:
+            all_runs.append(run);
+            run = [(entry[0], entry[2])]
+            current_limit = weight_limit - entry[1]
+    all_runs.append(run)
+    return all_runs
+
+def evaluate_permutation(perm):
+    runs = split_to_runs(perm)
+    return reduce(
+        lambda acc, run: run_around_distance(run) + acc,
+        runs, float(0)
+        ), runs
+
+def print_route(route, filename = 0):
+    if(filename == 0):
+        print('; '.join([recipient for recipient, point in run]))
+    else:
+        with open(sys.path[0] + filename, 'w') as f:
+            for run in route:
+                print('; '.join([recipient for recipient, point in run]), file=f)
+            
+
+data = []        
+with open(sys.path[0] + '/nicelist.txt', 'r') as file:
+    for line in file:
+        entry = line.strip().split(";")
+        data.append((entry[0], int(entry[3]), (float(entry[1]), float(entry[2]))))
+
+best = -1;
+best_route = []
+i = 1;
+for perm in itertools.permutations(data):
+    score, route = evaluate_permutation(perm)
+    if (score < best or best == -1): 
+        print("NEW BEST ROUTE FOUND ", str(i))
+        i = i + 1
+        best = score
+        best_route = route
+        print_route(best_route, '/best_route.csv')
+
+print('----------- BEGIN BEST ROUTE ----------')
+print_route(best_route)
+print('----------- END BEST ROUTE ------------')
 
